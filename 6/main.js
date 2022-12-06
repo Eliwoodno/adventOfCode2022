fda633b6c098652bb94feb85bb98f2e116544172
Pierre Barraux <pbarraux@gmail.com>
2022-12-06 23:04:10 +0100

    day 6

--- 6/main.js
+++ 6/main.js
@@ -4,7 +4,7 @@ const formattedInput = FS.readFileSync('./input', { encoding: 'utf-8'}).trim()
 
 
 const getMarkerIndex = (needleRange) => {
-    for (let i = 0; i < formattedInput.length - needleRange - 1; i++) {
+    for (let i = 0; i < formattedInput.length; i++) {
         const slice = formattedInput.slice(i, i + needleRange).split("")
         if(slice.length === new Set(slice).size) {
             return i + needleRange
@@ -14,5 +14,5 @@ const getMarkerIndex = (needleRange) => {
 // Part 1
 console.log(getMarkerIndex(4))
 
-//Part 2
+// Part 2
 console.log(getMarkerIndex(14))
